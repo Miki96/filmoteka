679f9e50f0e0675a704b24fbedf139f3bda81870
unknown <tasha996@bitbucket.org>
2019-01-24 23:49:30 +0100

    user modified

--- back/server.js
+++ back/server.js
@@ -29,16 +29,26 @@ client.connect(function (err) {
 	//client.close();
 });
 
-
+//test
 app.get('/', function (req, res) {
 	res.send('Hello World');
 });
 
-// save user
+// SIGN UP
 app.post('/user', function (req, res) {
-	console.log(req.body);
+	let username = (req.body.username == null) ? "" : req.body.username.trim();
+    let email = (req.body.email == null) ? "" : req.body.email.trim();
+    let pass = (req.body.pass == null) ? "" : req.body.pass.trim();
+   // let token = crypto.randomBytes(20).toString('hex');
+
+    if (username == "" || email == "" || pass == "") {
+        res.status(400).send('Form not valid');
+        return;
+	}
+	
 	db.collection('users').insertOne({
-		username: req.body.email,
+		username: req.body.username,
+		email: req.body.email,
 		password: req.body.pass
 	}, function (err, result) {
 		if (err) {
@@ -53,6 +63,37 @@ app.post('/user', function (req, res) {
 app.get('/users', function (req, res) {
 	db.collection('users').find().toArray(function (err, result) {
 		res.status(200).send(result);
+	});
+});
+
+// get user
+app.get('/user/:id', function (req, res) {
+	let ids = req.params.id;
+	console.log(ids);
+	var id = require('mongodb').ObjectID(ids);
+	
+	db.collection('users').findOne({
+		_id: id
+	}, function (err, result) {
+		if (err) {
+			res.status(400).send(err);
+		}
+		res.status(200).send(result);
+	});
+});
+
+// delete user
+app.delete('/user/:id', function (req, res) {
+	let ids = req.params.id;
+	var id = require('mongodb').ObjectID(ids);
+	
+	db.collection('users').remove({
+		_id: id
+	}, function (err, result) {
+		if (err) {
+			res.status(400).send(err);
+		}
+		res.status(200).send("deleted");
 	});
 });
 
